dcdb5711888ba1c4cb5f234f0740e912c7241f68
Artur Bermond Torres <41002679+denUlven@users.noreply.github.com>
2022-05-15 17:40:02 -0300

    mudei free

--- ordenacao.h
+++ ordenacao.h
@@ -13,7 +13,7 @@ typedef struct upa{
 
 UPA *upas_scan(int *qtd); //Aloca um vetor de upas e lé os dados de cada upa
 void upa_scan(UPA *upa); //Lé os dados de cada upa
-UPA *upas_free(UPA *upa, int qtd);//Libera o vetor das upas
+void *upas_free(UPA **upa, int qtd);//Libera o vetor das upas
 void upas_print(UPA *upa, int qtd); //Imprime as upas
 void upas_quicksort(UPA *upa, int l, int r); //Ordena as upas
 int upas_partition(UPA *upa, int l, int r); //Particiona as upas
